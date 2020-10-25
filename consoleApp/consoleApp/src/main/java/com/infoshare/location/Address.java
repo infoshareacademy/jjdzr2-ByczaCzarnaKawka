90306eaa1365638e7cd99132ab7ce58ea4da494b
michal-majewski <mimaj92@gmail.com>
2020-10-25 13:13:02 +0100

    close location package

--- consoleApp/consoleApp/src/main/java/com/infoshare/location/Address.java
+++ consoleApp/consoleApp/src/main/java/com/infoshare/location/Address.java
@@ -1,4 +1,31 @@
 package com.infoshare.location;
 
 public class Address {
+    private Town townName;
+    private LocalRoad localRoad;
+
+    public Address(Town townName) {
+        this.townName = townName;
+    }
+
+    public Address(Town townName, LocalRoad localRoad) {
+        this.townName = townName;
+        this.localRoad = localRoad;
+    }
+
+    public Town getTownName() {
+        return townName;
+    }
+
+    public void setTownName(Town townName) {
+        this.townName = townName;
+    }
+
+    public LocalRoad getLocalRoad() {
+        return localRoad;
+    }
+
+    public void setLocalRoad(LocalRoad localRoad) {
+        this.localRoad = localRoad;
+    }
 }
--- consoleApp/consoleApp/src/main/java/com/infoshare/location/Town.java
+++ consoleApp/consoleApp/src/main/java/com/infoshare/location/Town.java
@@ -9,5 +9,7 @@ public enum Town {
     Katowice,
     Kraków,
     Łódź,
-
+    Poznań,
+    Warszawa,
+    Wrocław
 }
