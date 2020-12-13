e18324f37506aeb9d12f10c7b6f2211e022722e4
michal-majewski <mimaj92@gmail.com>
2020-12-13 13:08:58 +0100

    Complete build - Maven and Spring Boot

--- consoleApp/consoleApp/src/main/java/com/infoshare/App.java
+++
@@ -1,8 +0,0 @@
-package com.infoshare;
-import com.infoshare.menu.Menu;
-
-public class App {
-    public static void main(String[] args) {
-        new Menu().start();
-    }
-}
---
+++ consoleApp/src/main/java/com/infoshare/App.java
@@ -0,0 +1,9 @@
+package com.infoshare;
+
+import com.infoshare.menu.Menu;
+
+public class App {
+    public static void main(String[] args) {
+        new Menu().start();
+    }
+}
