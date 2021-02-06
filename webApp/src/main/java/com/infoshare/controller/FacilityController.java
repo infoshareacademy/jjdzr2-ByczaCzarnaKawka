db1e9b56b12d7f4779e2a0e0a15ee8171fe5a112
michal-majewski <mimaj92@gmail.com>
2021-02-06 19:30:39 +0100

    Complete view for all facilities list

--- webApp/src/main/java/com/infoshare/controller/FacilityController.java
+++ webApp/src/main/java/com/infoshare/controller/FacilityController.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/facilities")
 public class FacilityController {
 
     private final FacilityService facilityService;
@@ -19,7 +21,7 @@ public class FacilityController {
         this.facilityService = facilityService;
     }
 
-    @GetMapping("/facilities/all")
+    @GetMapping("/list")
     public String getAllFacilityList(Model model){
         List<SportFacility> currentFacilitiesList = facilityService.getSportFacilityList();
 
