4dffc2280023edf91b99cfc453981c1d96d61635
Maria Derfel <mariaderfel@gmail.com>
2021-03-07 13:21:05 +0100

    views for admin and player

--- webApp/src/main/java/com/infoshare/controller/LoginController.java
+++ webApp/src/main/java/com/infoshare/controller/LoginController.java
@@ -1,7 +1,11 @@
 package com.infoshare.controller;
 
+import domain.users.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
@@ -16,6 +20,19 @@ public class LoginController {
 
     @GetMapping("/login")
     public String returnLoginForm(){
+        return "login";
+    }
+
+    //TODO : it is temporary endpoint, I need something to test playerView and adminView
+    @GetMapping("/login_send/{accessLevel}")
+    public String sendLogin(@PathVariable String accessLevel){
+
+        if("player".equals(accessLevel)){
+            return "playerView";
+        }
+        else if ("admin".equals(accessLevel)){
+            return "adminView";
+        }
         return "login";
     }
 
