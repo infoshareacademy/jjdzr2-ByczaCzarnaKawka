2ad8278fe6e305e35dbfec4bff919d6301eae900
Maria Derfel <mariaderfel@gmail.com>
2021-04-01 18:53:53 +0200

    SportFacilityRepository

--- serviceApp/src/main/java/repository/SportFacilityRepository.java
+++ serviceApp/src/main/java/repository/SportFacilityRepository.java
@@ -1,10 +1,21 @@
 package repository;
 
+import domain.activities.SportDiscipline;
+import domain.location.Town;
 import entity.SportFacilityEntity;
 import entity.id.SportFacilityID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SportFacilityRepository extends JpaRepository<SportFacilityEntity, SportFacilityID> {
+
+    List<SportFacilityEntity> findByAddressEntity_TownOrderByFacilityName(Town town);
+
+    List<SportFacilityEntity> findBySportDiscipline(SportDiscipline sportDiscipline);
+
+    List<SportFacilityEntity> findByFacilityNameContains(String name);
+
 }
